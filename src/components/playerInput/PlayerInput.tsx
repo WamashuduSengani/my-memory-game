61a2dc35c453b767acf04074f4725d69ce55512e
Wamashudu <Wamashudu.sengani@sovtech.com>
2024-03-14 19:22:51 +0200

    this change adds player names to local storage

--- src/components/playerInput/PlayerInput.tsx
+++ src/components/playerInput/PlayerInput.tsx
@@ -2,13 +2,17 @@ import React from "react";
 
 interface PlayerInputProps {
   placeholder: string;
+  value?: string;
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-const PlayerInput = ({ placeholder }: PlayerInputProps) => (
+const PlayerInput = ({ placeholder, value, onChange }: PlayerInputProps) => (
   <input
     type="text"
     placeholder={placeholder}
     className="player-name-input"
+    value={value}
+    onChange={onChange}
   />
 );
 
